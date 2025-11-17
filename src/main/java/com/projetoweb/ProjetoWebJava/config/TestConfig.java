d0348eff38b02143f8ae6fa5ec3ddc827b594054
PauloEduardo-Ferreira <paulo.eduardoliveira@hotmail.com>
2025-11-17 08:03:20 -0300

    OrderStatus enum

--- src/main/java/com/projetoweb/ProjetoWebJava/config/TestConfig.java
+++ src/main/java/com/projetoweb/ProjetoWebJava/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.projetoweb.ProjetoWebJava.entities.Order;
 import com.projetoweb.ProjetoWebJava.entities.User;
+import com.projetoweb.ProjetoWebJava.entities.enums.OrderStatus;
 import com.projetoweb.ProjetoWebJava.repositories.OrderRepository;
 import com.projetoweb.ProjetoWebJava.repositories.UserRepository;
 
@@ -30,9 +31,9 @@ public class TestConfig implements CommandLineRunner {
 		User user2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		User user3 = new User(null, "Juliana Ros", "juliana@gmail.com", "999922233", "30052024");
 		
-		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1); 
-		Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2); 
-		Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user3); 
+		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user1); 
+		Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2); 
+		Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, user3); 
 
 
 		userRepository.saveAll(Arrays.asList(user1, user2, user3));
