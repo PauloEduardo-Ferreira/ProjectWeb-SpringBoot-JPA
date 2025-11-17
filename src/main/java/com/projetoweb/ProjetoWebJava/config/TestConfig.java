e8afabac027a7392dfd618c5f38f2288a15621cc
PauloEduardo-Ferreira <paulo.eduardoliveira@hotmail.com>
2025-11-17 14:37:52 -0300

    Category

--- src/main/java/com/projetoweb/ProjetoWebJava/config/TestConfig.java
+++ src/main/java/com/projetoweb/ProjetoWebJava/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.projetoweb.ProjetoWebJava.entities.Category;
 import com.projetoweb.ProjetoWebJava.entities.Order;
 import com.projetoweb.ProjetoWebJava.entities.User;
 import com.projetoweb.ProjetoWebJava.entities.enums.OrderStatus;
+import com.projetoweb.ProjetoWebJava.repositories.CategoryRepository;
 import com.projetoweb.ProjetoWebJava.repositories.OrderRepository;
 import com.projetoweb.ProjetoWebJava.repositories.UserRepository;
 
@@ -23,9 +25,18 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics"); 
+		Category cat2 = new Category(null, "Books"); 
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 		User user1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
 		User user2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
@@ -34,7 +45,6 @@ public class TestConfig implements CommandLineRunner {
 		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user1); 
 		Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2); 
 		Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, user3); 
-
 
 		userRepository.saveAll(Arrays.asList(user1, user2, user3));
 		orderRepository.saveAll(Arrays.asList(order1, order2, order3));
