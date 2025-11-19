ca092c36ec16dcbfbb6667725e00576569788341
PauloEduardo-Ferreira <paulo.eduardoliveira@hotmail.com>
2025-11-19 14:55:29 -0300

    Exception handling - findById

--- src/main/java/com/projetoweb/ProjetoWebJava/services/UserService.java
+++ src/main/java/com/projetoweb/ProjetoWebJava/services/UserService.java
@@ -1,49 +1,57 @@
 package com.projetoweb.ProjetoWebJava.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projetoweb.ProjetoWebJava.entities.User;
 import com.projetoweb.ProjetoWebJava.repositories.UserRepository;
+import com.projetoweb.ProjetoWebJava.services.exceptions.ResourceNotFoundException;
 
 import jakarta.transaction.Transactional;
 
 @Service
 public class UserService {
 
-	@Autowired
-	private UserRepository repository;
-	
-	public List<User> findAll() {
-		return repository.findAll();
-	}
-	
-	public User findById(Long id) {
-		Optional<User> obj = repository.findById(id);
-		return obj.get();
-	}
-	
-	public User insert(User obj) {
-		return repository.save(obj);
-	}
-	
-	public void delete(Long id) {
-		repository.deleteById(id);
-	}
-	
-	@Transactional
-	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
-	}
-	
-	private void updateData(User entity, User obj) {
-		entity.setName(obj.getName());
-		entity.setEmail(obj.getEmail());
-		entity.setPhone(obj.getPhone());
-	}
+    @Autowired
+    private UserRepository repository;
+    
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+    
+    public User findById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+    
+    public User insert(User obj) {
+        return repository.save(obj);
+    }
+    
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+    
+    @Transactional
+    public User update(Long id, User obj) {
+
+        User entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(id));
+
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+    
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
 }
